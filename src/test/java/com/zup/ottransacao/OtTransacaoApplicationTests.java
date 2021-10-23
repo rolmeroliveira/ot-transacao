889f4b8ee3fbe65f672c98fdc6ac16f2d2b5c987
Rolmer Telis de Oliveira Telis <rolmer.oliveira@zup.com.br>
2021-10-23 06:08:42 -0300

    commit inicial

---
+++ src/test/java/com/zup/ottransacao/OtTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.ottransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
